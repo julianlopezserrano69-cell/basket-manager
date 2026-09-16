03c258c3ca3676d03901f9637f8163826528a7c6
julianlopezserrano69-cell <julian.lopez.serrano69@gmail.com>
2026-09-17 00:15:13 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'basket-manager-v8.5.143';
+const CACHE_NAME = 'basket-manager-v8.5.144';
 const APP_SHELL = ['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL))); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
